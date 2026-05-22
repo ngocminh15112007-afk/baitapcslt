4e28f201abbb349c708ed02af1bc0a5a2819bd00
LENOVO <LENOVO@DESKTOP-2JK808G>
2026-05-22 09:57:30 +0700

    a

---
+++ Baitapcslt/Buoi6/Ex5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baitapcslt.Buoi6
+{
+    public  class Ex5
+    {
+        public static void Max(int[] arr)
+        {
+           Random rand = new Random();
+           arr = new int[10];
+            int i = 0;
+            for (i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(10);
+            }
+            Console.WriteLine("Mang: ");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            int max = arr[0];
+            int min = arr[0];
+            foreach (int num in arr)
+            {
+                if (num > max)  max = num;
+                if (num < min) min = num;
+            }
+            Console.WriteLine(" "+max);
+            Console.WriteLine(" "+min);
+            return ;
+
+
+        }
+    }
+}
