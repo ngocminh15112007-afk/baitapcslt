a254a02bb80af46d318847be5b2add7e1eb2046f
LENOVO <LENOVO@DESKTOP-2JK808G>
2026-05-22 13:30:03 +0700

    aa

---
+++ Baitapcslt/Buoi6/Ex6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace baitapcslt.Buoi6
+{
+    public class Ex6
+    {
+        public static void Daomang(int[] arr)
+        {
+            Random rand = new Random();
+            arr = new int[10];
+            int i = 0;
+            for (i=0;i<arr.Length;i++)
+            {
+                arr[i] = rand.Next(10);
+            }
+            foreach (int num in arr)
+            {
+                Console.Write(num+" ");
+            }
+            // tao mang moi
+            int[] arr2 = new int[10];
+            int j = 0;
+
+            for (i=arr.Length-1 ;i>=0;i--)
+            {
+                arr2[j] = arr[i];
+                j++;
+            }
+            foreach (int num2 in arr2)
+            { Console.Write(num2 + " "); }
+            return;
+
+
+        }
+    }
+}
