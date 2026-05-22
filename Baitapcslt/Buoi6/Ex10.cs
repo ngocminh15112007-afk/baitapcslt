abd9678d30ed2d460246361778df2cca6a0fc651
LENOVO <LENOVO@DESKTOP-2JK808G>
2026-05-22 22:19:14 +0700

    a

---
+++ Baitapcslt/Buoi6/Ex10.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baitapcslt.Buoi6
+{
+    public class Ex10
+    {
+        public static void run34()
+        {
+            Console.WriteLine("Nhap vao mot cau: ");
+            string cau = Console.ReadLine();
+            Console.WriteLine("Nhap tu ban muon tim: ");
+            string tucantim = Console.ReadLine();
+            string[] mangcactu = cau.Split(' ');
+            bool timthay = false;
+            for (int i = 0; i<mangcactu.Length; i++)
+            {
+                if (mangcactu[i] == tucantim)
+                {
+                    timthay = true;
+                    break;
+                }
+            }
+            if (timthay = true)
+            {
+                Console.WriteLine("Co tu can tim");
+            }
+            else
+            {
+                Console.WriteLine("Khong co");
+            }
+        }
+            
+    }
+}
---
+++ Baitapcslt/Buoi6/Ex9.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baitapcslt.Buoi6
+{
+    public class Ex9
+    {
+        public static void Run33()
+        {
+            int[] arr = new int[10];
+            // yeu cau nhap 10 so
+            Console.WriteLine("Nhap 10 so nguyen: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"So thu {i+1}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i= 0; i<arr.Length -1; i++)
+            {
+                for (int j = 0; j<arr.Length-1-i;j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];     
+                        arr[j] = arr[j + 1];     
+                        arr[j + 1] = temp;       
+                    }
+                }
+            }
+            Console.WriteLine("\nMang sau khi sap xep tang dan:");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
+
+
+
+       
