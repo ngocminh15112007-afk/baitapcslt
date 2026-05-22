53bcf55de279d6b55174b292461fc20c8ea358d0
LENOVO <LENOVO@DESKTOP-2JK808G>
2026-05-22 20:55:58 +0700

    aaaaaa

---
+++ Baitapcslt/Buoi6/Ex7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baitapcslt.Buoi6
+{
+    public class Ex7
+    {
+        public static void Timsogiong(int[] arr)
+        {
+            Random rand = new Random();
+            arr = new int[10];
+            int i = 0;
+            for (i=0;i<arr.Length;i++)
+            {
+                arr[i] = rand.Next(10);
+            }
+            foreach(int num in arr)
+            Console.Write($"Day so la:{num} + ");
+
+            //tim so giong
+            int j = 0;
+            int[] arr2 = new int[10];
+            for (i = 0; i<arr.Length; i++) 
+            { 
+                for (j =0; j<arr2.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Baitapcslt/Buoi6/Ex8.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace baitapcslt.Buoi6
+{
+    public class Ex8
+    {
+        public static void Xoasogiong(int[] arr)
+        {
+            Random rand = new Random(10);
+            arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(10);
+            }
+            foreach (int num in arr)
+                Console.Write(num + " ");
+            // taoj mảng mới để đựng các số duy nhất, kthuoc = tối đa mãng cũ
+
+            int[] arr2 = new int[arr.Length];
+            int j = 0;
+            //đếm lượng phần từ duy nhất
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool daxuathien = false;
+
+
+                for (int k = 0; k < j; k++)
+                {
+                    if (arr[i] == arr2[k])
+                    {
+                        daxuathien = true;
+                        break;
+                    }
+
+                }
+                if (daxuathien == false)
+                {
+                    arr2[j] = arr[i];
+                    j++;
+                }
+            }
+            int[] mangchinh = new int[j];
+            for (int i = 0; i < j; i++)
+            {
+                mangchinh[i] = arr2[j];
+                Console.WriteLine(mangchinh[i] + " ");
+            }
+        }
+
+
+         
+
+    }
+}
